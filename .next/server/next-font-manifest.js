d3947156b3143dfb9c56e1788d195891abfadea8
Icannotcodelol <maxhenkes@web.de>
2025-04-12 19:04:18 +0200

    Update Stripe config, add logging, apply DB schema

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{"/_app":["static/media/a34f9d1faa5f3315-s.p.woff2"]},"app":{"/Users/maxhenkes/Desktop/ValiNow-REPAIR/src/app/layout":["static/media/a34f9d1faa5f3315-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":true}';
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/maxhenkes/Desktop/ValiNow-REPAIR/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
